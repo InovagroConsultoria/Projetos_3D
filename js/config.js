875416c2af3845c081374637d47172b36e7de922
Thomas Augusto Klotz <117672548+ThomasAK47@users.noreply.github.com>
2026-07-20 14:46:33 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -60,7 +60,7 @@ export const TALUDES = [
     },
     {
         nome: "Talude T-6",
-        glb:  "Superficie/t6t7/t6_superficie.glb",
+        glb:  "Superficie/t6t7/t6.glb",
         csv:  "Superficie/t6t7/Pontos_t6.csv",
         data: "",
         vista: { flipH: true },
