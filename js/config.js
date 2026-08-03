6de22de5dc49ad01673663301f7ef44fa55f8a4c
Thomas Augusto Klotz <117672548+ThomasAK47@users.noreply.github.com>
2026-08-03 11:42:47 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -84,7 +84,7 @@ export const TALUDES = [
         nome: "Talude T-8",
         glb:  "Superficie/t8/t8_0604.glb",
         csv:  "Superficie/t8/Total_t8.csv",
-        data: "16/07/2026",
+        data: "01/08/2026",
         vista: { flipH: true, guide: [{ e: 231559.111, n: 6716071.921 }, { e: 231543.551, n: 6716083.173 }, { e: 231528.469, n: 6716104.958 }, { e: 231526.314, n: 6716129.615 }, { e: 231528.948, n: 6716157.864 }, { e: 231543.551, n: 6716183.000 }, { e: 231632.366, n: 6716251.706 }] },
     },
     {
@@ -100,7 +100,7 @@ export const TALUDES = [
         nome: "Talude T-10",
         glb:  "Superficie/t_10/superficie_t10.glb",
         csv:  "Superficie/t_10/Pontos_t10_visu.csv",
-        data: "01/06/2026",
+        data: "01/08/2026",
         vista: { flipH: true },
     },
     {
